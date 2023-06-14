f4679780674df24336c938dda4bc98f989101f0a
Alexandre Marra <alexsmarra@hotmail.com>
2023-06-14 09:26:17 -0300

    AppError: Criando AppError para lidar com erros e inserindo-o em nosso UsersController, especificamente na function create

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors')
-
+const AppError = require("./utils/AppError")
 
 const express = require('express')
 
@@ -8,6 +8,24 @@ const routes = require("./routes")
 const app = express()
 app.use(express.json())
 app.use(routes)
+
+app.use((err, req, res, next) => {
+   // client side
+   if(err instanceof AppError) {
+      return res.status(err.statusCode).json({
+         status: 'error',
+         message: err.message
+      })
+   }
+
+   console.log(err)
+
+   //server side
+   return res.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+   })
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}.`))
