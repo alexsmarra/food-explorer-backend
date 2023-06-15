372b816a8dbb603ef015fc3dca86ced9d83cd859
Alexandre Marra <alexsmarra@hotmail.com>
2023-06-15 15:53:02 -0300

    SessionController: testando validacao de senha e email pelo Insomnia

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,6 +1,27 @@
+/* File for user authentication, if it is authenticated (with email and password
+typed correctly), we will generate a token for it */
+
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs")
+
 class SessionController {
    async create(req, res) {
-      return res.json()
+      const { email, password } = req.body
+
+      const user = await knex('users').where({ email }).first()
+
+      if(!user) {
+         throw new AppError('Wrong e-mail or password!', 401)
+      }
+
+      const passwordMatched = await compare(password, user.password)
+
+      if(!passwordMatched) {
+         throw new AppError("Password or email doesn't match!", 401)
+      }
+
+      return res.json(user)
    }
 }
 
